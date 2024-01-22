72d894f5626f2ed3d10ef59fba986b5250fb19d0
oscar066 <oscarkaruga1@gmail.com>
2024-01-22 08:46:39 +0300

    Deleted App folder

---
+++ Frontendd/src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './header.css'
+
+function Header() {
+  return (
+    <div className='navbar'>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#contact">Contact</a>
+        <a href="#blog">Blog</a>
+    </div>
+  )
+}
+
+export default Header;
+
