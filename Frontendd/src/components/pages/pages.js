174085a5e6ed986afc99f30f824ee14d713d9db0
oscar066 <oscarkaruga1@gmail.com>
2024-01-29 13:02:23 +0300

    Added the pages for Routing

---
+++ Frontendd/src/components/pages/pages.js
@@ -0,0 +1,42 @@
+
+import React from "react"
+
+export function Home(){
+    return (
+        <div>
+            <h1>[Home]</h1>
+        </div>
+    );
+}
+
+export function About(){
+    return (
+        <div>
+            <h1>[About]</h1>
+        </div>
+    );
+}
+
+export function Events(){
+    return (
+        <div>
+            <h1>[Events]</h1>
+        </div>
+    );
+}
+
+export function products(){
+    return (
+        <div>
+            <h1>[Products]</h1>
+        </div>
+    );
+}
+
+export function Contact(){
+    return (
+        <div>
+            <h1>[Contact]</h1>
+        </div>
+    );
+}
