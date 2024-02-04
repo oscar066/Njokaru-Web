7399e7a486176fcde9cbd0bae9d8c33be9d2508c
oscar066 <oscarkaruga1@gmail.com>
2024-02-04 23:51:45 +0300

    :Upadate App and Index

--- Frontendd/src/App.js
+++ Frontendd/src/App.js
@@ -6,12 +6,14 @@ import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import Cards from './components/Cards/Cards'
 import Contact from './components/Contact/Contact';
+import CarouselFadeExample from './components/Hero/Carousel';
 
 function App() {
   return (
     <div className="App">
      <Header />
      <Hero />
+     <CarouselFadeExample />
      <AboutUs />
      <Cards />
      <Contact />
