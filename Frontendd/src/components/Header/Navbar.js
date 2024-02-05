e7f88714bdead904d8006847c0026f80dbb639ae
oscar066 <oscarkaruga1@gmail.com>
2024-02-05 10:15:19 +0300

    :lipstick: Added Navbar

---
+++ Frontendd/src/components/Header/Navbar.js
@@ -0,0 +1,30 @@
+
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+
+function CustomNavbar() {
+  return (
+    <Navbar expand="lg" className="navbar">
+      <Container>
+        <Navbar.Brand href="#home">
+          <span className='company-name'>NJOKARU</span>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto"></Nav>
+          <Nav>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#services">Services</Nav.Link>
+            <Nav.Link href="#blog">Blog</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default CustomNavbar;
