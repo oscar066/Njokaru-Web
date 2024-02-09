86653a93aa1db0a9aae017878207c86eede7ec18
oscar066 <oscarkaruga1@gmail.com>
2024-02-09 11:45:24 +0300

    :created the ProductPage

---
+++ Frontendd/src/components/pages/ProductPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from '../Footer/Footer'
+import Navbar from '../Header/Navbar'
+
+function ProductPage() {
+  return (
+    <div className='BlogPage'>
+        <Navbar />
+        <div>
+            <h2>Products page Coming soon ... </h2>
+        </div>
+        <Footer />
+    </div>
+  )
+}
+
+export default ProductPage
