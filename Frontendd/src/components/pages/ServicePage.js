59475301cde5f98c820fa607d7638455d3594e16
oscar066 <oscarkaruga1@gmail.com>
2024-02-09 11:45:48 +0300

    :created the ServicePage

---
+++ Frontendd/src/components/pages/ServicePage.js
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import Footer from '../Footer/Footer'
+import Navbar from '../Header/Navbar'
+
+function ServicePage() {
+  return (
+    <div className='ServicePage'>
+        <Navbar />
+        <div>
+            <h2>Service page Coming soon ... </h2>
+        </div>
+        <Footer />
+    </div>
+  )
+}
+
+export default ServicePage
