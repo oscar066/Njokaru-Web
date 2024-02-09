8834444825cd8587864e03cbd3b7e8df09126fc1
oscar066 <oscarkaruga1@gmail.com>
2024-02-09 11:44:45 +0300

    :created the BlogPage

---
+++ Frontendd/src/components/pages/BlogPage.js
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import Footer from '../Footer/Footer'
+import Navbar from '../Header/Navbar'
+
+function BlogPage() {
+  return (
+    <div className='BlogPage'>
+        <Navbar />
+        <div>
+            <h2>Blog page Coming soon ... </h2>
+        </div>
+        <Footer />
+    </div>
+  )
+}
+
+export default BlogPage
