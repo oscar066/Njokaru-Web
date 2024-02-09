559e19fd5a0da0ca48ec9209a8116914dbbc9ae2
oscar066 <oscarkaruga1@gmail.com>
2024-02-09 11:47:07 +0300

    :lipstick: ContactPage

---
+++ Frontendd/src/components/pages/ContactPage.js
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import Footer from '../Footer/Footer'
+import Navbar from '../Header/Navbar'
+
+function ContactPage() {
+  return (
+    <div className='ContactPage'>
+        <Navbar />
+        <div>
+            <h2>Contact page Coming soon ... </h2>
+        </div>
+        <Footer />
+    </div>
+  )
+}
+
+export default ContactPage
