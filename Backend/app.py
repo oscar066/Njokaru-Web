1781afbd5b7fc824af890eea8fa14d4837784ba0
oscar066 <oscarkaruga1@gmail.com>
2024-02-29 12:56:40 +0300

    :construction:Backend Mail server build v1

--- Backend/app.py
+++ Backend/app.py
@@ -8,13 +8,16 @@ app = Flask(__name__)
 CORS(app)
 
 # Configure Flask Mail
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 25
+app.config['MAIL_SERVER'] = 'live.smtp.mailtrap.io'
+app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = 'oscarkaruga1@gmail.com'
+#app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+#app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_USERNAME'] = 'api'
+app.config['MAIL_PASSWORD'] = 'f29741c4bf7f75906b99c21041460f77'
+app.config['MAIL_DEFAULT_SENDER'] = 'mailtrap@demomailtrap.com'
+
 
 mail = Mail(app)
 
@@ -31,12 +34,16 @@ def get_data():
 def contact():
     data = request.json  # Assuming you're sending JSON data from the React app
 
-    msg = Message('New Contact Form Submission',
-                  recipients=['your_admin@example.com'])  # Change to your email
+    recipient_email = data.get('email')
+
+    if not recipient_email:
+        return jsonify({'success': False, 'message': 'Email address not provided.'})
+
+    msg = Message('New Contact Form Submission', recipients=[recipient_email])
 
     msg.body = f'''
     Full Name: {data.get('fullName')}
-    Email: {data.get('email')}
+    Email: {recipient_email}
     Phone Number: {data.get('phoneNumber')}
     Service Type: {data.get('serviceType')}
     Message: {data.get('message')}
@@ -48,6 +55,6 @@ def contact():
     except Exception as e:
         print(e)
         return jsonify({'success': False, 'message': 'Failed to send email.'})
-
+    
 if __name__ == '__main__':
     app.run(debug=True)
