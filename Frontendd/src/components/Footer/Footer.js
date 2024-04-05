5eee46689308e4eecf57c4ae1a7bccdfff954239
oscar066 <oscarkaruga1@gmail.com>
2024-04-05 13:22:06 +0300

    :lipstick: Improved the UI of footer copyright section still an issue

--- Frontendd/src/components/Footer/Footer.js
+++ Frontendd/src/components/Footer/Footer.js
@@ -6,28 +6,44 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className='footer'>
-      <div className='footer-content'>
-        <h3>Gardening & Maintenance</h3>
-        <p>Creating and maintaining beautiful outdoor spaces.</p>
-      </div>
-      <div className='footer-section'>
-        <h4>Contact Us</h4>
-        <p>Email: njokaru@gardening.com</p>
-        <p>Phone: +254 719686913</p>
-      </div>
-      <div className='footer-section'>
-        <h4>Follow Us</h4>
-        <div className='social-icons'>
-          <a href='https://www.facebook.com/' target='_blank' rel='noopener noreferrer'>
-            <FaFacebook />
-          </a>
-          <a href='https://twitter.com/' target='_blank' rel='noopener noreferrer'>
-            <FaTwitter />
-          </a>
-          <a href='https://www.instagram.com/' target='_blank' rel='noopener noreferrer'>
-            <FaInstagram />
-          </a>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-3'>
+            <h4>Gardening & Maintenance</h4>
+            <p>Creating and maintaining beautiful outdoor spaces.</p>
+          </div>
+          <div className='col-md-3'>
+            <h4>Contact Us</h4>
+            <p>Email: njokaru@gardening.com</p>
+            <p>Phone: +254 719686913</p>
+          </div>
+          <div className='col-md-3'>
+            <h4>Follow Us</h4>
+            <div className='social-icons'>
+              <a href='https://www.facebook.com/' target='_blank' rel='noopener noreferrer'>
+                <FaFacebook />
+              </a>
+              <a href='https://twitter.com/' target='_blank' rel='noopener noreferrer'>
+                <FaTwitter />
+              </a>
+              <a href='https://www.instagram.com/' target='_blank' rel='noopener noreferrer'>
+                <FaInstagram />
+              </a>
+            </div>
+          </div>
+          <div className='col-md-3'>
+            <h4>Links</h4>
+            <ul className="list-unstyled">
+              <li><a href="/">Home</a></li>
+              <li><a href="/services">Services</a></li>
+              <li><a href="/about">About</a></li>
+              <li><a href="/contact">Contact</a></li>
+            </ul>
+          </div>
         </div>
+      </div>
+      <div className='container text-center copyright-section'>
+        <p className="copyright">&copy; {new Date().getFullYear()} Gardening & Maintenance. All rights reserved.</p>
       </div>
     </footer>
   );
