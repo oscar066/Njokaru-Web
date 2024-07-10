b296ff8689c6d7207089575ef8ce841b90615063
oscar066 <oscarkaruga1@gmail.com>
2024-07-10 16:14:40 +0300

    :fire: Removed build environment

--- Frontendd/src/components/Footer/Footer.js
+++ Frontendd/src/components/Footer/Footer.js
@@ -1,54 +1,49 @@
+
 import React from 'react';
-import { FaFacebook, FaTwitter, FaInstagram, FaPhone, FaEnvelope } from 'react-icons/fa';
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 import './Footer.css';
-
-const footerLinks = [
-  { label: 'Home', href: '/' },
-  { label: 'Services', href: '/services' },
-  { label: 'About', href: '/about' },
-  { label: 'Contact', href: '/contact' },
-];
-
-const socialLinks = [
-  { icon: FaFacebook, href: 'https://www.facebook.com/' },
-  { icon: FaTwitter, href: 'https://twitter.com/' },
-  { icon: FaInstagram, href: 'https://www.instagram.com/' },
-];
 
 function Footer() {
   return (
     <footer className='footer'>
-      <div className='footer-content'>
-        <div className='footer-section'>
-          <h4>Gardening & Maintenance</h4>
-          <p>Creating and maintaining beautiful outdoor spaces.</p>
-        </div>
-        <div className='footer-section'>
-          <h4>Contact Us</h4>
-          <p><FaEnvelope /> njokaru@gardening.com</p>
-          <p><FaPhone /> +254 719686913</p>
-        </div>
-        <div className='footer-section'>
-          <h4>Follow Us</h4>
-          <div className='social-icons'>
-            {socialLinks.map((link, index) => (
-              <a key={index} href={link.href} target='_blank' rel='noopener noreferrer' aria-label={`Visit our ${link.icon.name.replace('Fa', '')}`}>
-                <link.icon />
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-3'>
+            <h4>Gardening & Maintenance</h4>
+            <p>Creating and maintaining beautiful outdoor spaces.</p>
+          </div>
+          <div className='col-md-3'>
+            <h4>Contact Us</h4>
+            <p>Email: njokaru@gardening.com</p>
+            <p>Phone: +254 719686913</p>
+          </div>
+          <div className='col-md-3'>
+            <h4>Follow Us</h4>
+            <div className='social-icons'>
+              <a href='https://www.facebook.com/' target='_blank' rel='noopener noreferrer'>
+                <FaFacebook />
               </a>
-            ))}
+              <a href='https://twitter.com/' target='_blank' rel='noopener noreferrer'>
+                <FaTwitter />
+              </a>
+              <a href='https://www.instagram.com/' target='_blank' rel='noopener noreferrer'>
+                <FaInstagram />
+              </a>
+            </div>
+          </div>
+          <div className='col-md-3'>
+            <h4>Links</h4>
+            <ul className="list-unstyled">
+              <li><a href="/">Home</a></li>
+              <li><a href="/services">Services</a></li>
+              <li><a href="/about">About</a></li>
+              <li><a href="/contact">Contact</a></li>
+            </ul>
           </div>
         </div>
-        <div className='footer-section'>
-          <h4>Quick Links</h4>
-          <ul className="footer-links">
-            {footerLinks.map((link, index) => (
-              <li key={index}><a href={link.href}>{link.label}</a></li>
-            ))}
-          </ul>
-        </div>
       </div>
-      <div className='copyright-section'>
-        <p>&copy; {new Date().getFullYear()} Gardening & Maintenance. All rights reserved.</p>
+      <div className='container text-center copyright-section'>
+        <p className="copyright">&copy; {new Date().getFullYear()} Gardening & Maintenance. All rights reserved.</p>
       </div>
     </footer>
   );
--- Frontendd/src/components/Header/Navbar.js
+++ Frontendd/src/components/Header/Navbar.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+
+import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -6,32 +7,16 @@ import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 function CustomNavbar() {
-  const [scrolled, setScrolled] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const isScrolled = window.scrollY > 10;
-      if (isScrolled !== scrolled) {
-        setScrolled(isScrolled);
-      }
-    };
-
-    document.addEventListener('scroll', handleScroll);
-
-    return () => {
-      document.removeEventListener('scroll', handleScroll);
-    };
-  }, [scrolled]);
-
   return (
-    <Navbar expand="lg" className={`navbar ${scrolled ? 'scrolled' : ''}`} fixed="top">
+    <Navbar expand="lg" className="navbar">
       <Container>
         <Link to="/" className="navbar-brand">
           <span className='company-name'>NJOKARU</span>
         </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ms-auto nav-links">
+          <Nav className="me-auto"></Nav>
+          <Nav className='nav-links'>
             <Link to="/" className="nav-link">Home</Link>
             <Link to="/about" className="nav-link">About</Link>
             <Link to="/services" className="nav-link">Services</Link>
