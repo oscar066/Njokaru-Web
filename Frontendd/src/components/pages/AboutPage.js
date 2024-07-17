fc8fd9533d002d3ec05521eab2f79bb56c9102ad
oscar066 <oscarkaruga1@gmail.com>
2024-07-18 00:59:18 +0300

    :lipstick: Redesigned the About Page (#2)

--- Frontendd/src/components/pages/AboutPage.js
+++ Frontendd/src/components/pages/AboutPage.js
@@ -3,12 +3,14 @@ import React from 'react'
 import Footer1 from '../Footer/Footer'
 import Navbar from '../Header/Navbar'
 import AboutUs from '../AboutUs/AboutUs'
+import WhyUs from '../WhyUs/WhyUs'
 
 function AboutPage() {
   return (
     <div className='AboutPage'>
         <Navbar />
         <AboutUs />
+        <WhyUs />
         <Footer1 />
     </div>
   )
