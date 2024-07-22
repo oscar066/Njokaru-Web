b15a8da4fef4844366cb7eeb2c7efbeedb4d46a3
oscar066 <oscarkaruga1@gmail.com>
2024-07-23 00:41:36 +0300

    :construction: Addde transition btw Carousel slides

--- Frontendd/src/components/Hero/Carousel.js
+++ Frontendd/src/components/Hero/Carousel.js
@@ -36,7 +36,7 @@ const ImprovedCarousel = () => {
   };
 
   useEffect(() => {
-    const timer = setInterval(nextSlide, 5000);
+    const timer = setInterval(nextSlide, 10000);
     return () => clearInterval(timer);
   }, []);
 
