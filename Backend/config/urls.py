6540370610145433062bd9b6bf34b4faf218f9d0
oscar066 <oscarkaruga1@gmail.com>
2024-07-30 01:04:11 +0300

    :construction: Including product urls in the project urls

--- Backend/config/urls.py
+++ Backend/config/urls.py
@@ -1,24 +1,20 @@
-"""
-URL configuration for config project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('api/', include('accounts.urls')),
-]
+    path('api/accounts/', include('accounts.urls')),  
+    path('api/products/', include('products.urls')),  
+
+    # path('api/docs/', TemplateView.as_view(template_name='swagger-ui.html'), name='api-docs'), 
+      # API documentation
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Custom error handlers
+#handler404 = 'config.views.custom_404_view'
+#handler500 = 'config.views.custom_500_view'
