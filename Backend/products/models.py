ef82038d70b018cc907e1ee7f7be3fa82e3a73f4
oscar066 <oscarkaruga1@gmail.com>
2024-07-30 01:01:05 +0300

    :construction: Products Model (#1)

--- Backend/products/models.py
+++ Backend/products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField(upload_to='products/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
