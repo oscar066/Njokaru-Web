832ea8bf424ab3ca4457b1069b0005bcb7fc40b7
oscar066 <oscarkaruga1@gmail.com>
2024-08-14 01:56:13 +0300

    :construction: Contact page

---
+++ frontend-next/src/app/contacts/page.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const ContactPage: React.FC = () => {
+    return (
+      <div className="pt-16"> 
+        <h1 className="text-3xl text-center">Calling the contact Page</h1>
+      </div>
+    );
+  };
+  
+  export default ContactPage;  
