114066dfa155ee81ac38ca01a73c6bda95bfa383
oscar066 <oscarkaruga1@gmail.com>
2024-08-14 01:49:45 +0300

    :construction: Update on the Home Page

--- frontend-next/src/app/page.tsx
+++ frontend-next/src/app/page.tsx
@@ -1,7 +1,14 @@
 import Image from "next/image";
+import React from 'react';
+import ImprovedCarousel from "./components/Hero/Carousel";
 
-export default function Home() {
+const HomePage: React.FC = () => {
   return (
-    <h1>Hello Oscar</h1>
+    <div className="pt-16"> 
+      <ImprovedCarousel />
+      <h1 className="text-3xl text-center">Hello World from Home Page</h1>
+    </div>
   );
-}
+};
+
+export default HomePage;
