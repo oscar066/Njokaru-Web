ed7baa539b2547b53e97f4f001da9b5b0160f09e
oscar066 <oscarkaruga1@gmail.com>
2024-08-14 01:59:22 +0300

    :construction: About Page

---
+++ frontend-next/src/app/about/page.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const AboutPage: React.FC = () => {
+    return (
+      <div className="pt-16"> 
+        <h1 className="text-3xl text-center">About Page</h1>
+      </div>
+    );
+  };
+  
+  export default AboutPage;
