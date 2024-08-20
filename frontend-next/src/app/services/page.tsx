4a9a659b800e81d4f4de09c9a9416e03790c1305
oscar066 <oscarkaruga1@gmail.com>
2024-08-20 16:58:11 +0300

    :construction: Services component into the Services page

--- frontend-next/src/app/services/page.tsx
+++ frontend-next/src/app/services/page.tsx
@@ -1,9 +1,11 @@
+
 import React from 'react';
+import Services from '../components/Services/services';
 
 const ServicesPage: React.FC = () => {
     return (
-      <div className="pt-16"> 
-        <h1 className="text-3xl text-center">Services Page</h1>
+      <div className="pt-8"> 
+        <Services />
       </div>
     );
   };
