95784ea4e736027467a626066242cd8238dc18ea
oscar066 <oscarkaruga1@gmail.com>
2024-08-26 22:48:30 +0300

    :lipstick: Changed to font-serif

--- frontend-next/src/app/components/Footer/Footer.tsx
+++ frontend-next/src/app/components/Footer/Footer.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer: React.FC = () => {
     return (
-        <footer className='bg-white text-gray py-4'>
-            <div className='container mx-auto px-4 text-center'>
+        <footer className='bg-white text-gray-800 py-4 font-serif'>
+            <div className='container mx-auto px-4 text-center text-sm'>
                 <p>&copy; {new Date().getFullYear()} NJOKARU. All rights reserved.</p>
             </div>
         </footer>
