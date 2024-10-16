737ffcf58abdf111691317efe42122bd002134b9
oscar066 <oscarkaruga1@gmail.com>
2024-10-16 19:42:10 +0300

    :construction: Used the services page component to replace services component

--- frontend-next/src/app/services/page.tsx
+++ frontend-next/src/app/services/page.tsx
@@ -1,11 +1,12 @@
 
 import React from 'react';
 import Services from '../components/Services/services';
+import ServicesPage2 from '../components/Services/ServicesPage';
 
 const ServicesPage: React.FC = () => {
     return (
       <div className="pt-8"> 
-        <Services />
+        <ServicesPage2 />
       </div>
     );
   };
