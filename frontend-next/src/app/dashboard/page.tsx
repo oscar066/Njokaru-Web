1ede28d048ed63c0781c01235fc19280f480221e
oscar066 <oscarkaruga1@gmail.com>
2024-10-22 13:11:16 +0300

    :construction: Content Management system for products

---
+++ frontend-next/src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+import Dashboard from '../components/Products-Dashboard/dashboard';
+
+const ContactPage: React.FC = () => {
+    return (
+      <div className="pt-18"> 
+        <Dashboard />
+      </div>
+    );
+  };
+  
+  export default ContactPage;  
