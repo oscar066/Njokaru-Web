7d13aab9dbf93af40e8a7f18b5878540b8176223
oscar066 <oscarkaruga1@gmail.com>
2024-10-22 13:33:48 +0300

    :construction: Auth removed deleted components

--- frontend-next/src/app/auth/signup/page.tsx
+++ frontend-next/src/app/auth/signup/page.tsx
@@ -1,10 +1,6 @@
 'use client';
 
 import React from 'react';
-import SignupHeader from '../../components/Signup/signupHeader';
-import SignupForm from '../../components/Signup/signupForm';
-import SignupSideImage from '../../components/Signup/signupSideImage';
-import SocialSignupButton from '../../components/Signup/socialSignupButton';
 import SignupPage from '@/app/components/Signup/signup';
 
 const Signup: React.FC = () => {
