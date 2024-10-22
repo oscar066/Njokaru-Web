7305803105fd9aee1a028429e66364aabf30ecdd
oscar066 <oscarkaruga1@gmail.com>
2024-10-22 13:08:11 +0300

    :construction: Added Forget Password

---
+++ frontend-next/src/app/auth/forget-password/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+import ForgetPassword from '@/app/components/Forget-password/forget-password';
+
+const Login: React.FC = () => {
+  return (
+    <div className="flex min-h-screen bg-white">
+         <ForgetPassword />
+    </div>
+  );
+};
+
+export default Login;
