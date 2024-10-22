cd0d5c4a186735f869dffccab1933c3eb919caf1
oscar066 <oscarkaruga1@gmail.com>
2024-10-22 13:33:20 +0300

    :construction: Auth removed deleted components

--- frontend-next/src/app/auth/login/page.tsx
+++ frontend-next/src/app/auth/login/page.tsx
@@ -1,10 +1,6 @@
 'use client';
 
 import React from 'react';
-import LoginSideImage from '../../components/Login/loginSideImage';
-import LoginHeader from '../../components/Login/loginHeader';
-import LoginForm from '../../components/Login/loginForm';
-import SocialLoginButton from '../../components/Login/socialLoginButton';
 import LoginPage from '@/app/components/Login/login';
 
 const Login: React.FC = () => {
