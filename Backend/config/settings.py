77617ee91810458c6f0934a545cd606838d2d026
oscar066 <oscarkaruga1@gmail.com>
2024-11-12 14:37:09 +0300

    :construction: Add Orders to local apps and allow no auth for products rest endpoint for test

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -53,6 +53,8 @@ INSTALLED_APPS = [
     'accounts',
     'products',
     'blogs',
+    'orders',
+
 ]
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
@@ -62,7 +64,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES' : [
-        'rest_framework.permissions.IsAuthenticated',
+        #'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
 }
 
