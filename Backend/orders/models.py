56ecd808b3a2f15784b8b4713e7f5f80dcdbc7ad
oscar066 <oscarkaruga1@gmail.com>
2024-11-12 14:34:31 +0300

    :construction: Initial Orders app

---
+++ Backend/orders/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.conf import settings
+from products.models import Product
+
+# Order models
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    order_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50, choices=[('pending', 'Pending'), ('shipped', 'Shipped'), ('delivered', 'Delivered')])
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Order {self.id} by {self.user.username}"
+    
+class OrederItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price_at_purchase = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.product.name} (x{self.quantity}) in Order {self.order.id}"
