45d350c62a9bcc2e6565de6d8691538aba2a4461
oscar066 <oscarkaruga1@gmail.com>
2024-11-12 14:43:58 +0300

    :construction: checkout page

---
+++ frontend-next/src/app/checkout/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Checkout from '../components/Products/checkout'
+
+const CheckoutPage: React.FC = () => {
+    return (
+      <div className="pt-8"> 
+        <Checkout />
+      </div>
+    );
+  };
+  
+  export default CheckoutPage;
