20fe98d6ea9f18267ee58731a7b8f628a3c7c2ac
oscar066 <oscarkaruga1@gmail.com>
2024-11-12 14:41:49 +0300

    :construction: Add confirmation page

---
+++ frontend-next/src/app/order-confirmation/page.tsx
@@ -0,0 +1,14 @@
+
+import react from "react"
+
+import ConfirmationPage from '../components/Products/confirmation'
+
+const ConfirmPage: React.FC = () => {
+    return (
+      <div className="pt-8"> 
+        <ConfirmationPage />
+      </div>
+    );
+  };
+  
+  export default ConfirmPage;
