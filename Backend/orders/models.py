6512ad887b1b8dcfb67584a8b3d590ec4d94f282
oscar066 <oscarkaruga1@gmail.com>
2024-11-14 19:15:04 +0300

    :construction: correct typo

--- Backend/orders/models.py
+++ Backend/orders/models.py
@@ -13,7 +13,7 @@ class Order(models.Model):
     def __str__(self):
         return f"Order {self.id} by {self.user.username}"
     
-class OrederItem(models.Model):
+class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
