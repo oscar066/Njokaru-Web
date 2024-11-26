d56f929b1f6e49a2cd58ba489b7b1e5baa128e91
oscar066 <oscarkaruga1@gmail.com>
2024-11-26 17:56:05 +0300

    :construction: About us components

--- frontend-next/src/app/about/page.tsx
+++ frontend-next/src/app/about/page.tsx
@@ -1,24 +1,16 @@
 "use client";
 
 import React from "react";
-// import AboutUs from '../components/About/about';
+
 import AboutHero from "../components/About/about-hero";
-// import OurStory from '../components/About/about-our-story';
-import AboutUsStory from "../components/About/about-us-story";
-// import TeamSection from '../components/About/about-team';
-// import FAQSection from '../components/About/about-faq';
-// import Team from '../components/Team/team';
-import OurStory from "../components/About/about-our-story";
+// import AboutUsStory from "../components/About/about-us-story";
+import AboutUs from "../components/About/about-us";
 
 const AboutPage: React.FC = () => {
   return (
     <div className="pt-8">
       <AboutHero />
-      {/* <AboutUs /> */}
-      <OurStory />
-      <AboutUsStory />
-      {/* <Team /> */}
-      {/* <FAQSection /> */}
+      <AboutUs />
     </div>
   );
 };
